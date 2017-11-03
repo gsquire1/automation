114e4596d8a88662f90e0b904db30eec1fd084e4
root <gsquire@brocade.com>
2017-11-03 15:23:10 -0600

    adding files

---
+++ FS/GS_REST.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+
+
+import requests
+import json
+import  re
+import os
+import sys
+
+#sys.path.append ("/opt/python/lib/python3.4/site-packages/")
+###################################
+###################################
+
+def _url(path):
+    """
+    """
+    return('http://10.38.36.33'  + path)
+
+
+def add_task(summary, description=""):
+    """
+    """
+    return(requests.post(_url('/rest/login')))
+
+def get_tasks():
+    """
+    """
+    return(request.get(_url('')))
+    
+    
+def main():
+    
+    r = requests.post("http://10.38.36.33/rest/login", auth=('admin','fibranne'))
+    print("RJSON1111111111111111111111111111111111111111")
+    print(r.json)
+    print("RSTATUSCODE2222222222222222222222222222222222")
+    print(r.status_code)
+    print("RTEXT3333333333333333333333333333333333333333")
+    print(r.text)
+    print('RHEADERS4444444444444444444444444444444444444')
+    print(r.headers)
+    print('RCOOKIES555555555555555555555555555555555')
+    print(r.cookies)
+    Auth = r.headers.get('Authorization')
+    #session = r.session()
+    print(Auth)
+    
+    Auth_send={'Authorization':'%s'%Auth}
+    print(Auth_send)
+    
+    
+    ###########################################################################
+    ###########################################################################
+    ####
+    ####   Container        switch
+    ####     list                  fibrechannel-switch
+    ####     
+    ####     leaf                   domain-id
+    ####
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch", headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+    ras = re.compile('name>([\:0-9a-f]+)</n')
+    ras = ras.findall(r.text)
+    print(type(ras))
+    print(ras)
+    wwn = ras[0]
+
+    # r = requests.get("http://10.38.36.33/rest/running/switch/zoning" % wwn , headers=Auth_send)
+    # print(r.json)
+    # print(r.text)
+    # print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/domain-id" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+    
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/user-friendly-name" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/fcid" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/vf-id" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/principal" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/enabled-state" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/up-time" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/model" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/firmware-version" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/ip-address" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/domain-name" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/fabric-user-friendly-name" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/ag-mode" % wwn , headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+
+###############################################################################
+###############################################################################
+####
+####          LOGOUT           LOGOUT
+####
+###############################################################################
+
+    r = requests.post("http://10.38.36.33/rest/logout", headers=Auth_send)
+    #print(r.json)
+    print(r.status_code)
+    
+    sys.exit()
+
+
+
+
+
+
+
+
+    r = requests.get("http://10.38.36.33/rest/running/zoning/effective-configuration", headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    r = requests.get("http://10.38.36.33/rest/running/fabric/fabric-switch", headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    
+    r = requests.get("http://10.38.36.33/rest/running/brocade-interface/fibrechannel-statistics", headers=Auth_send)
+    print(r.json)
+    print(r.text)
+    print(r.headers)
+
+    
+    ###########################################################
+    ###########################################################
+    #### to run the up-time request you need the wwn of the switch
+    ####   the first request get the info to capture the wwn
+    ####
+    #### 
+    print("#"*120)
+    print("Fibrechannel-switch")
+    print("#"*120)  
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch", headers=Auth_send)
+    print(r.json)
+    print(r.status_code)
+    print(r.text)
+    print(r.headers)
+    print('@'*80)
+    print(r.encoding)
+    ras = re.compile('name>([\:0-9a-f]+)</n')
+    ras = ras.findall(r.text)
+    print(type(ras))
+    print(ras)
+    wwn = ras[0]
+    print(type(wwn))
+    print(wwn)
+    print("#"*120)
+    print("Fibrechannel-switch/up-time")
+    print("#"*120)
+    r = requests.get("http://10.38.36.33/rest/running/switch/fibrechannel-switch/name/%s/up-time" % wwn, headers=Auth_send)
+    #print(r.json)
+    #print(r.text)
+    #print(r.headers)
+    if r.status_code != 200:
+        print("error with the request")
+        print(r.text)
+    else:
+        pass
+    
+    
+
+
+
+    r = requests.post("http://10.38.36.33/rest/logout", headers=Auth_send)
+    #print(r.json)
+    print(r.status_code)
+    
+#10.38.34.192
+if __name__ == '__main__':
+    
+    main()
