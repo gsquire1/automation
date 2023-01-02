ac30012b6f16f426134a5e57221e834a3deb0ef4
Graham Squire <gsquire1@msn.com>
2023-01-01 17:18:06 -0700

    Added New Learning document. Literal Constructor

---
+++ Learning_LiteralConstructor.py
@@ -0,0 +1,9 @@
+# Dictionary Literal Constructor
+
+my_dict = {'key1': 'val1', 'key2': 123, 'key3': [0, 1, 2]}
+# literal constructor below
+my_dict1 = dict(key1=123, key2=[12, 23, 33], key3=['item0', 'item1', 'item2'])
+my_dict2 = {'key1': 123, 'key2': [12, 23, 33], 'key3': ['item0', 'item1', 'item2']}
+print(my_dict)
+print(my_dict1)
+print(my_dict2)
